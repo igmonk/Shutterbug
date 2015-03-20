4c87aaa4a21d4756cd1b1f47c000e5f32d1e2753
Igor Moiseyenko <imoiseyenko93@gmail.com>
2015-03-20 16:59:00 +0300

    Initial commit - project set up.

---
+++ Shutterbug/FlickrPhotosTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FlickrPhotosTableViewController.h
+//  Shutterbug
+//
+//  Created by Igor on 3/20/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FlickrPhotosTableViewController : UITableViewController
+
+@end
---
+++ Shutterbug/FlickrPhotosTableViewController.m
@@ -0,0 +1,51 @@
+//
+//  FlickrPhotosTableViewController.m
+//  Shutterbug
+//
+//  Created by Igor on 3/20/15.
+//  Copyright (c) 2015 Igor. All rights reserved.
+//
+
+#import "FlickrPhotosTableViewController.h"
+
+@interface FlickrPhotosTableViewController ()
+
+@end
+
+@implementation FlickrPhotosTableViewController
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
